420d0ba5f441ea21392b1ba32758ffcae4c76f15
Ivan Sukhanov <ivan.sukhanov@noveogroup.com>
2022-10-10 01:53:26 +0400

    Added frontend connection + first request handling

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
+import { Message } from '../types/Messages';
 
-@Controller('rooms')
+@Controller('room')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @Get(':id')
-  find(@Param('id') id: string): string {
-    return `This action returns a #${id} room`;
+  @Get()
+  find(): Message[] {
+    return [
+      {
+        id: 0,
+        text: 'Hello!',
+        userId: Date.now() + '',
+        timeSent: Date.now(),
+      },
+      {
+        id: 1,
+        text: 'How are you?',
+        userId: Date.now() + '',
+        timeSent: Date.now(),
+      },
+    ];
   }
 }
