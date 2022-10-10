3e90860b3289d53e87d6562509c262e92f0658fe
Ivan Sukhanov <ivan.sukhanov@noveogroup.com>
2022-10-10 22:26:00 +0400

    Room endpoints updated

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { Message } from '../types/Messages';
 
@@ -6,21 +6,13 @@ import { Message } from '../types/Messages';
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @Get()
-  find(): Message[] {
-    return [
-      {
-        id: 0,
-        text: 'Hello!',
-        userId: Date.now() + '',
-        timeSent: Date.now(),
-      },
-      {
-        id: 1,
-        text: 'How are you?',
-        userId: Date.now() + '',
-        timeSent: Date.now(),
-      },
-    ];
+  @Get(':id')
+  getMessages(@Param('id') id: string): Message[] {
+    return this.roomsService.getMessages(id);
+  }
+
+  @Post()
+  createRoom(@Body() body: { id: string }) {
+    this.roomsService.createRoom(body);
   }
 }
